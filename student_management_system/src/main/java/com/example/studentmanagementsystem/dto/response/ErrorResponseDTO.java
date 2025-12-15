53c754d23b78f57d98a2cf3ef8f8e9c10e768ffb
KhanhTaiTran <tktai2003@gmail.com>
2025-12-15 17:36:15 +0700

    add: contants

---
+++ student_management_system/src/main/java/com/example/studentmanagementsystem/dto/response/ErrorResponseDTO.java
@@ -0,0 +1,5 @@
+package com.example.studentmanagementsystem.dto.response;
+
+public class ErrorResponseDTO {
+    
+}
--- student_management_system/src/main/java/com/example/studentmanagementsystem/utils/AppConstants.java
+++ student_management_system/src/main/java/com/example/studentmanagementsystem/utils/AppConstants.java
@@ -2,4 +2,26 @@ package com.example.studentmanagementsystem.utils;
 
 public class AppConstants {
 
+    // --- 1. PAGINATION ---
+    // default page number 0 (first page)
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+
+    // default take 10 records per page
+    public static final String DEFAULT_PAGE_SIZE = "10";
+
+    // default sorting 'id'
+    public static final String DEFAULT_SORT_BY = "id";
+    // asc
+    public static final String DEFAULT_SORT_DIRECTION = "asc";
+
+    // --- 2. SECURITY & JWT ---
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    public static final String BEARER_TOKEN_PREFIX = "Bearer ";
+
+    // --- 3. BUSINESS LOGIC ---
+    public static final String MIN_BALANCE_TO_ENROLL = "500000";
+
+    private AppConstants() {
+    }
 }
