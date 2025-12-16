06d5a27681e6b71e7fbb211d94eec036f7cd9bff
KhanhTaiTran <tktai2003@gmail.com>
2025-12-16 17:56:46 +0700

    feature/getAll and create course

--- student_management_system/src/main/java/com/example/studentmanagementsystem/controller/CourseController.java
+++ student_management_system/src/main/java/com/example/studentmanagementsystem/controller/CourseController.java
@@ -1,5 +1,34 @@
 package com.example.studentmanagementsystem.controller;
 
-public class CourseController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.studentmanagementsystem.entity.Course;
+import com.example.studentmanagementsystem.service.CourseService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("api/admin/courses")
+public class CourseController {
+    private CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        return ResponseEntity.ok(courseService.createCourse(course));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+        return ResponseEntity.ok(courseService.getAllCourses());
+    }
 }
