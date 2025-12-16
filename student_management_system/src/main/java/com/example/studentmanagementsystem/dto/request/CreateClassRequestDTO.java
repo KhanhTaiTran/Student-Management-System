7a87b6004e75d7e52c24527e5a45d412e40e633a
KhanhTaiTran <tktai2003@gmail.com>
2025-12-16 18:00:03 +0700

    add/ createClassrequestDTO

---
+++ student_management_system/src/main/java/com/example/studentmanagementsystem/dto/request/CreateClassRequestDTO.java
@@ -0,0 +1,18 @@
+package com.example.studentmanagementsystem.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateClassRequestDTO {
+    private String className;
+    private Long courseId;
+    private Long teacherId;
+    private String semester;
+    private String classRoom; // e.g. A1.202
+}
